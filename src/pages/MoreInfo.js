b3447b4228422836e9cce8781db44206b234922d
Angelina <vuangelina72@gmail.com>
2021-03-09 03:19:28 -0800

    Update MoreInfo.js

--- src/pages/MoreInfo.js
+++ src/pages/MoreInfo.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from "gatsby";
 
 const MoreInfo = () => (
   <div>
     <h1>NOT FOUND</h1>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
     <p>buttons</p>
   </div>
 )
