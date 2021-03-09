e89efd819f34801d07baca2c8ae0d831f8b568ae
Angelina <vuangelina72@gmail.com>
2021-03-09 03:25:22 -0800

    Update buttons.js

--- src/pages/buttons.js
+++ src/pages/buttons.js
@@ -5,9 +5,10 @@ const switches = () => (
     <h1>Normally Open (NO) vs Normally Closed (NC) Switches</h1>
     <p>Swicthes are devices that either complete or break the flow of electricity. Generally, switches have two states (opened and closed). Here is a switch in an open state.</p>
     <p>Here is a switch in a closed state.</p>
+    <img src="pic_trulli.jpg" alt="Italian Trulli">
     <p>As you can see, the connection is broken when the button is in an open state. This means that electricity will not be able to flow through the button. On the other hand, the connection is complete when the button is in a closed state, allowing the electricity to flow.
     </p>
-    <p>s </p>
+    <img src="https://static-cdn.imageservice.cloud/7019537/what-is-a-proximity-sensor-wolf-automation.png" alt="NO  vs NC">
   </div>
 )
 
