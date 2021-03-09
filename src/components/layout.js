865bddadf31e667f9131eb9249739a45d42209a5
Angelina <vuangelina72@gmail.com>
2021-03-09 02:55:09 -0800

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -66,7 +66,7 @@ const TemplateWrapper = ({ children }) => {
                   <Link to="/">Projects</Link>
                 </li>
                 <li>
-                  <Link to="/moreinfo">More Information</Link>
+                  <Link to="/moreinfo">Learn More</Link>
                 </li>
                 <li>
                   <Link to="/about">About</Link>
