f8ff773089e3fef32d960996b61d29757d9431bf
Angelina <vuangelina72@gmail.com>
2021-03-09 03:18:06 -0800

    Create buttons.js

---
+++ src/pages/buttons.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const switches = () => (
+  <div>
+    <h1>Normally Open (NO) vs Normally Closed (NC) Switches</h1>
+    <p>Swicthes are devices that either complete or break the flow of electricity. Generally, switches have two states (opened and closed). Here is a switch in an open state.</p>
+    <p>Here is a switch in a closed state.</p>
+    <p>As you can see, the connection is broken when the button is in an open state. This means that electricity will not be able to flow through the button. On the other hand, the connection is complete when the button is in a closed state, allowing the electricity to flow.
+    </p>
+    <p>s </p>
+  </div>
+)
+
+export default switches
