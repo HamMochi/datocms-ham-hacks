a34d7306f0a580ed7218eddef58a9b3f36746e08
Angelina <vuangelina72@gmail.com>
2021-03-09 03:32:00 -0800

    Update buttons.js

--- src/pages/buttons.js
+++ src/pages/buttons.js
@@ -7,9 +7,7 @@ const switches = () => (
     <img src="https://scontent.fsac1-2.fna.fbcdn.net/v/t1.15752-9/158858710_1358036657894341_797699633280457321_n.png?_nc_cat=105&ccb=1-3&_nc_sid=ae9488&_nc_ohc=IpbAOhJihY8AX8Tskne&_nc_ht=scontent.fsac1-2.fna&oh=bb39c73690e6efd2b9f9dbbf3efcd8a7&oe=606B9A68" alt="Open Switch">
     <p>Here is a switch in a closed state.</p>
     <img src="https://scontent.fsac1-1.fna.fbcdn.net/v/t1.15752-9/158038156_4099060143439362_4723908251725078153_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=ae9488&_nc_ohc=dTAnhkWb33cAX8zoDRh&_nc_ht=scontent.fsac1-1.fna&oh=76745893359c748334164354a4b7f485&oe=606EA809" alt="Closed Switch">
-    <p>As you can see, the connection is broken when the button is in an open state. This means that electricity will not be able to flow through the button. On the other hand, the connection is complete when the button is in a closed state, allowing the electricity to flow.
-    </p>
-    <img src="https://static-cdn.imageservice.cloud/7019537/what-is-a-proximity-sensor-wolf-automation.png" alt="NO  vs NC">
+    <p>As you can see, the connection is broken when the button is in an open state. This means that electricity will not be able to flow through the button. On the other hand, the connection is complete when the button is in a closed state, allowing the electricity to flow.</p>
   </div>
 )
 
