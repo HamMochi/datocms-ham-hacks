8e1ee7d7695baca1d1148e563ea7f0486e026367
Angelina <vuangelina72@gmail.com>
2021-03-09 03:51:44 -0800

    Update MoreInfo.js

--- src/pages/MoreInfo.js
+++ src/pages/MoreInfo.js
@@ -5,7 +5,19 @@ const MoreInfo = () => (
   <div>
     <h1>NOT FOUND</h1>
       <li>
+        <Link to="/Arduino">Arduinos</Link>
+      </li>
+      <li>
+        <Link to="/batteries">Batteries</Link>
+      </li>
+      <li>
+        <Link to="/motors">Motors</Link>
+      </li>
+      <li>
         <Link to="/switches">Switches</Link>
+      </li>
+      <li>
+        <Link to="/transformers">Transformers</Link>
       </li>
     <p>buttons</p>
   </div>
