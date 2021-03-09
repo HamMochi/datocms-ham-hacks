f38327b7a3e6b2aef95875885175fca847b95ab4
Angelina <vuangelina72@gmail.com>
2021-03-09 02:40:43 -0800

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -63,7 +63,7 @@ const TemplateWrapper = ({ children }) => {
               />
               <ul className="sidebar__menu">
                 <li>
-                  <Link to="/">Home</Link>
+                  <Link to="/">Ham Home</Link>
                 </li>
                 <li>
                   <Link to="/about">About</Link>
